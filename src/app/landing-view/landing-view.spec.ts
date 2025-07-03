2408709360ffc1dedf7aeaf638294b97e9d1e51a
BUILD-MONK <dixitsohan427@gmail.com>
2025-07-04 00:11:33 +0530

    Added the test cases

--- src/app/landing-view/landing-view.spec.ts
+++ src/app/landing-view/landing-view.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingView } from './landing-view';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('LandingView', () => {
   let component: LandingView;
@@ -8,7 +9,8 @@ describe('LandingView', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingView]
+      imports: [LandingView],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
 
--- src/app/shared.service.spec.ts
+++ src/app/shared.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SharedService } from './shared.service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('SharedService', () => {
   let service: SharedService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()]
+    });
     service = TestBed.inject(SharedService);
   });
 
